768838787e768c8dc253e3a6effb818f9dccd19e
rishikeshupadhya345 <51327802+rishikeshupadhya345@users.noreply.github.com>
2021-10-01 12:13:39 +0530

    Update wt.js

--- wt.js
+++ wt.js
@@ -1,51 +1,54 @@
+function display(){
+    
 
-function getflights(){
+    var w=document.getElementById("from").value  
+    console.log(w)
+    var x=document.getElementById("to").value
+    fetch(`https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/?query=${w}`, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+            "x-rapidapi-key": "7fb6e6e027mshdb4b803bc116c32p1b254fjsndf9b4e669c03"
+        }
+    })
+    .then(response => response.json()
+    ).then(data => {
+        console.log(data);
+        console.log(data.Places);
+        data.Places.forEach(element => {
+            console.log(element);
+            document.getElementById("Start").innerHTML+= ` <option>${element.PlaceName}</option>`;
+        });
+        
+    }
+    )
+    .catch(err => {
+        console.error(err);
+    })
     
-var w=document.getElementById("from").value  
-console.log(w)
-var x=document.getElementById("to").value
-fetch(`https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/?query=${w}`, {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-		"x-rapidapi-key": "7fb6e6e027mshdb4b803bc116c32p1b254fjsndf9b4e669c03"
-	}
-})
-.then(response => response.json()
-).then(data => {
-    console.log(data);
-    console.log(data.Places);
-    data.Places.forEach(element => {
-        console.log(element);
-        document.getElementById("Start").innerHTML+= ` <option>${element.PlaceName}</option>`;
-    });
+    fetch(`https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/?query=${x}`, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+            "x-rapidapi-key": "7fb6e6e027mshdb4b803bc116c32p1b254fjsndf9b4e669c03"
+        }
+    })
+    .then(response => response.json()
+    ).then(data => {
+        console.log(data);
+        console.log(data.Places);
+        data.Places.forEach(element => {
+            console.log(element);
+            document.getElementById("end").innerHTML+= ` <option>${element.PlaceName}</option>`
+        });
+        
+    }
+    )
+    .catch(err => {
+        console.error(err);
+    })
     
-}
-)
-.catch(err => {
-	console.error(err);
-})
 
-fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/?query="+w, {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-		"x-rapidapi-key": "7fb6e6e027mshdb4b803bc116c32p1b254fjsndf9b4e669c03"
-	}
-})
-.then(response => response.json()
-).then(data => {
-    console.log(data);
-    console.log(data.Places);
-    data.Places.forEach(element => {
-        console.log(element);
-        document.getElementById("end").innerHTML+= ` <option>${element.PlaceName}</option>`
-    });
-    
-}
-)
-.catch(err => {
-	console.error(err);
-})
+
 
 }
