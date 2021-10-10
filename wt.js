33d3386db8c5b2dfc244103155b82b1e6d872745
rishikeshupadhya345 <51327802+rishikeshupadhya345@users.noreply.github.com>
2021-10-10 11:48:38 +0530

    Update wt.js

--- wt.js
+++ wt.js
@@ -17,11 +17,11 @@ function display(){
             </div>
             <div style="margin-top: -48px;margin-left: 885px;font-size: 18px;">
                 <span>Date</span>
-                <input type="date">
+                <input type="date" id="date">
             </div>
             <div style="margin-top: 110px; margin-left: 75px; font-size: 18px;">
                 <span>Number of passengers</span>
-                <input type="number" min="1">
+                <input type="number" id="no" min="1">
             </div>
             <div style="margin-top: -34px; margin-left: 740px; font-size: 18px;">
                 <span>Seat Type</span>
@@ -33,7 +33,7 @@ function display(){
                 </select>     
             </div>
             <div style="margin-top: 88px; margin-left: 543px;font-size: 25px;">
-                <input type="button" style="border-radius: 9px;" value="Check Flights">
+                <input type="button" style="border-radius: 9px;" value="Check Flights" onclick="display2()">
             </div>
           </form>
         </div>   
@@ -52,10 +52,8 @@ document.getElementById("content").innerHTML+=data;
     })
     .then(response => response.json()
     ).then(data => {
-        console.log(data);
-        console.log(data.Places);
+        
         data.Places.forEach(element => {
-            console.log(element);
             document.getElementById("Start").innerHTML+= ` <option>${element.PlaceName}</option>`;
         });
         
@@ -74,10 +72,8 @@ document.getElementById("content").innerHTML+=data;
     })
     .then(response => response.json()
     ).then(data => {
-        console.log(data);
-        console.log(data.Places);
+        
         data.Places.forEach(element => {
-            console.log(element);
             document.getElementById("end").innerHTML+= ` <option>${element.PlaceName}</option>`
         });
         
@@ -91,3 +87,71 @@ document.getElementById("content").innerHTML+=data;
 
 
 }
+async function display2(){
+    var l=document.getElementById("Start").value
+    var m=document.getElementById("end").value
+    var city1="";
+    var city2="";
+    const x=`<table id="ans" border="2">
+    <th>Source &rarr; Destination</th>
+    <th>Airlines</th>
+    <th>Price</th>
+    <th>Total Price</th>
+    
+</table>`
+
+
+    await fetch(`https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/?query=${l}`, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+            "x-rapidapi-key": "7fb6e6e027mshdb4b803bc116c32p1b254fjsndf9b4e669c03"
+        }
+    })
+    .then(response=>response.json()).then(data=>{
+        city1=data.Places[0].CityId
+        console.log(city1)
+
+        //localStorage.setItem("city1",data.Places[0].CityId)
+        //console.log(localStorage.getItem("city1"))
+
+    }).catch(err => {
+        console.error(err);
+    })
+    await fetch(`https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/?query=${m}`, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+            "x-rapidapi-key": "7fb6e6e027mshdb4b803bc116c32p1b254fjsndf9b4e669c03"
+        }
+    })
+    .then(response=>response.json()).then(data=>{
+        // window.city2=data.Places[0].CityId
+        city2=data.Places[0].CityId
+        console.log(city2)
+        //localStorage.setItem("city2",data.Places[0].CityId)
+        //console.log(localStorage.getItem("city2"))
+        
+
+    }).catch(err => {
+        console.error(err);
+    })
+    fetch(`https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/INR/en-US/${city2}/${city1}/${document.getElementById("date").value}`, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+            "x-rapidapi-key": "7fb6e6e027mshdb4b803bc116c32p1b254fjsndf9b4e669c03"
+        }
+        })
+    .then(response => response.json()).then(data=>{
+        var a=0
+        data.Carriers.forEach(element=>{
+        document.getElementById("ans").innerHTML+=`<tr><td>${l}&rarr;${m}</td><td>${element.Name}</td><td>${data.Quotes[a].MinPrice}</td><td>${data.Quotes[a].MinPrice*document.getElementById("no").value}</td></tr>`
+        a=a+1
+    })})
+    .catch(err => {
+        console.error(err);
+    });
+    document.getElementById("w").innerHTML+=x;
+}
+
